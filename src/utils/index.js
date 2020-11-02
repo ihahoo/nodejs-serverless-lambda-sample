c06df15135d8e1ad6d92dd200c39b1968907582b
Ray Guo <ihahoo@163.com>
2020-11-02 15:31:06 +0800

    判断符合条件数据的工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+const validator = require('validator');
 const AWS = require('aws-sdk');
 const { v1: uuidv1 } = require('uuid');
 const config = require('../config');
@@ -7,6 +8,22 @@ AWS.config.update({ region: 'us-west-1' });
 const s3 = new AWS.S3();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS();
+
+// 是否是符合条件的数据
+const isCorrectDataType = (record) => record && Array.isArray(record) && record.length === 3
+  && validator.isLatLong(`${record[0]},${record[1]}`) && record[2].length > 0;
+
+// 是否是符合条件的文件，（取前三条和最后一条数据判断是否是符合条件的数据)
+const isCorrectDataFile = (records) => {
+  let result = false;
+  if (records && Array.isArray(records)) {
+    const selectrecords = records.slice(0, 3);
+    if (records.length > 3) selectrecords.push(records[records.length - 1]);
+
+    result = selectrecords.every((v) => isCorrectDataType(v));
+  }
+  return result;
+};
 
 // 从s3读取csv文件
 const readCsvFile = async (bucket, key) => {
@@ -53,6 +70,8 @@ const sendErrorMsg = async (msg) => {
 };
 
 module.exports = {
+  isCorrectDataType,
+  isCorrectDataFile,
   readCsvFile,
   saveData,
   sendErrorMsg,
